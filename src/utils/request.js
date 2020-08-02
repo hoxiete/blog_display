b6201a7eb4210d8685103cb4c153ea00fc781437
zhanghao <787326576@qq.com>
2020-08-02 16:45:25 +0800

    test

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,9 +4,9 @@ import store from '@/store'
 export const imgBaseUrl = "http://resource.hoxiete.cn/"
 // create an axios instance
 const service = axios.create({
-     baseURL: process.env.VUE_APP_BASE_API,
+    baseURL: process.env.VUE_APP_BASE_API,
     // baseURL: "http://localhost:8088/blogShow",
-    //  baseURL: "http://www.hoxiete.cn/zhwtf/blogShow",
+    // baseURL: "http://www.hoxiete.cn/zhwtf/blogShow",
     timeout: 5000 // request timeout
 })
 
